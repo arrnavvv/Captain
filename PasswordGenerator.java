9e70ad984ff81d81e1e45336ee3e3fe9b5403236
arrnavvv <61576354+arrnavvv@users.noreply.github.com>
2020-09-15 20:33:45 +0530

    Add files via upload
    A Simple Password Generator which follows the basic rules of a strong password namely:
        1. Password has to be at least 6 characters long.
        2. Must contain at least 
                2.aone lower case letter,
               2.b one upper case letter,
               2.c one digit
               2.done of these special characters ~!@#$%^&*()_+

---
+++ PasswordGenerator.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
+class Generate{
+    String smallAlphabet="abcdefghijklmnopqrstuvwxyz";
+    String largeAlphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    String digits="1234567890";
+    String specialCharacters="!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    String all = smallAlphabet+largeAlphabet+digits+specialCharacters;
+
+    public String generatePassword(int length){
+        Random random = new Random();
+        StringBuilder password = new StringBuilder();
+                password.append((smallAlphabet.charAt(random.nextInt(26)))).
+                        append(largeAlphabet.charAt(random.nextInt(26))).
+                        append(digits.charAt(random.nextInt(10))).
+                        append(specialCharacters.charAt(random.nextInt(31)));
+        int left = length-4;
+        StringBuilder remPass = new StringBuilder();
+        while(left-->0){
+            remPass.append(all.charAt(random.nextInt(93)));
+        }
+        password.append(remPass);
+        ArrayList<Character> list = new ArrayList<>();
+        for(int i=0;i<password.toString().length();i++){
+            list.add(password.charAt(i));
+        }
+        Collections.shuffle(list);
+        StringBuilder finalPassword = new StringBuilder();
+        for(int i=0;i<list.size();i++){
+            finalPassword.append(list.get(i));
+        }
+        return finalPassword.toString();
+    }
+}
+
+
+public class PasswordGenerator {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        Generate generate = new Generate();
+        System.out.println("Enter the length of the password you require: ");
+        int lenOfPass = scanner.nextInt();
+        if(lenOfPass<6 || lenOfPass>32){
+            while(lenOfPass<6||lenOfPass>32) {
+                System.out.println("Please enter length greater than 6 and less than 32");
+                lenOfPass=scanner.nextInt();
+            }
+        }
+        String password = generate.generatePassword(lenOfPass);
+        System.out.println("Your password is: "+password);
+    }
+}
